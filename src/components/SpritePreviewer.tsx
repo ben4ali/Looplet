b0136f70dd41402517086bbdf5c2d64f9485dcd9
Ali <alibenkarrouch@gmail.com>
2025-04-08 23:13:36 -0400

    Added intial components

---
+++ src/components/SpritePreviewer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SpritePreviewer = () => {
+    return (
+        <div>
+            <h1>Ello</h1>
+        </div>
+    );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { SpritePreviewer } from "../components/SpritePreviewer"
+
+export const Home = () => {
+  return (
+    <main className="p-4">
+      <h1 className="text-3xl font-bold mb-6">Looplet</h1>
+      <SpritePreviewer />
+    </main>
+  )
+}
+
